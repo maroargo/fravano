4f1be6711a127b69d4c04eac8b362714cf7eaaa6
Manuel Arrisueno <oti158@sunedu.gob.pe>
2025-04-16 18:39:02 -0500

    Ajustes

--- app/(protected)/analytic/page.tsx
+++ app/(protected)/analytic/page.tsx
@@ -2,7 +2,7 @@ export default async function Analytic() {
     
   return (
     <>
-      <iframe title="TRANSPORTATION APP" width="1140" height="541.25" 
+      <iframe title="FRAVANO APP" width="1140" height="541.25" 
         src="https://app.powerbi.com/reportEmbed?reportId=96537384-f80f-4e02-9a69-7362673d98f2&autoAuth=true&ctid=62481a46-4946-41f5-af46-76afeb29a49c"></iframe>
     </>  
     
